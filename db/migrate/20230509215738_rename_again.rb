6e616d97df20f38e146b24879e978fec1be39b54
Alex Slyusarev <aslyusarev@gmail.com>
2023-05-11 10:17:07 -0700

    updated names

--- db/migrate/20230509215738_rename_again.rb
+++ db/migrate/20230509215738_rename_again.rb
@@ -1,5 +1,5 @@
 class RenameAgain < ActiveRecord::Migration[7.0]
   def change
-    rename_column :users, :password_hash, :new_column
+    rename_column :users, :password_hash, :Password_digest
   end
 end
