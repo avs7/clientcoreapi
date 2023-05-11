c0360bb2d8576b8bba5792ad2e8383fba93c89b0
Alex Slyusarev <aslyusarev@gmail.com>
2023-05-11 14:14:43 -0700

    added create action for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,20 @@
 class UsersController < ApplicationController
-  before_action :ensure_authorized!
+  before_action :ensure_authorized!, except: %[create]
 
   def index
     @user = current_user
   end
-  
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    else
+      render @user.errors
+    end
+  end
+
+  private
+  def user_params
+    params.permit(:first_name, :last_name, :email, :password, :organization, :title, :bio)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :users
   post '/auth/login', to: 'authentication#create'
-
   resource :user, only: %i[index]
+
+  post '/signup', to: 'users#create'
 
 end
